633aae4e8684cc822e5298ccc518c55df38822ce
Levy Araujo <levyxaraujo@gmail.com>
2022-05-30 18:30:33 -0300

    Search module

---
+++ server/csv_search.py
@@ -0,0 +1,23 @@
+import csv
+import sys
+import pandas as pd
+
+#  função que retorna o resultado da busca do usuário no CSV
+def search(key):
+    headers = None
+    values = None
+    filtered = []
+    if key == None:
+        return None
+    with open("relatorio.csv", "r") as csv:
+        for index, row in enumerate(csv):
+            if index == 0:
+                headers = row.rstrip("\n").split(",")
+            if key.upper() in str(row).upper():
+                values = row.rstrip("\n").split(",")
+                filtered.append(dict(zip(headers, values)))
+
+    return filtered
+
+
+# print(search("para"))
