8ad2c0ab8dbaf6d0129832f7dcd2724ae5757939
Levy Araujo <levyxaraujo@gmail.com>
2022-06-01 16:53:30 -0300

    Resolve imports

--- server/server.py
+++ server/server.py
@@ -1,17 +1,11 @@
 from flask import Flask, request, jsonify
 from flask_restful import Api, Resource
 from flask_cors import CORS
-from csv_search import search
+from .csv_search import search
 
 app = Flask(__name__)
 api = Api(app)
 CORS(app, resources={r"/*": {"origins": "*"}})
-
-
-class HelloWorld(Resource):
-    def get(self, key):
-        print(request.args)
-        return {"hello": "world"}
 
 
 class SearchData(Resource):
@@ -21,8 +15,4 @@ class SearchData(Resource):
         return jsonify(result)
 
 
-api.add_resource(HelloWorld, "/")
 api.add_resource(SearchData, "/search")
-
-
-app.run(debug=True)
